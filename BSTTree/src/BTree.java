c3eb39e3e3e32d5a45aee7ff2508ba04ef2bcac0
Always-Swiftie <mebuster06@gmail.com>
2025-03-26 22:46:34 +0800

    Finished

--- BSTTree/src/BTree.java
+++ BSTTree/src/BTree.java
@@ -118,5 +118,86 @@ public class BTree {
 
         parent.keyCount++;
     }
+
+    public void remove(int key){
+        doRemove(null,root,0,key);
+    }
+
+    private void doRemove(Node parent,Node node,int index,int key){
+        int i=0;
+        while (i<node.keyCount){
+            if(node.keys[i]>=key){
+                //found or go to child
+                break;
+            }
+            i++;
+        }
+        //如果当前节点没找到，那要去退出时的i下标的的孩子节点
+        //如果找到，i就是待删除key的索引
+        if(node.isLeaf) {
+            if (i < node.keyCount && node.keys[i] == key) {//found&&isLeaf case2
+                node.removeKey(i);
+            } else {//not found&&isLeaf case1
+                return;
+            }
+        }else{
+            if (i < node.keyCount && node.keys[i] == key) {//found&&NotLeaf  case4
+                //先找后继，再替换
+                Node s=node.children[i+1];
+                while (!s.isLeaf){
+                    s=s.children[0];//向左走
+                }
+                int skey=s.keys[0];//找到后继key
+                node.keys[i]=skey;
+                doRemove(node,node.children[i+1],i+1,skey);
+            } else {//not found&&NotLeaf case3
+                doRemove(node,node.children[i],i,key);
+            }
+        }
+        if(node.keyCount<MIN_KEY_NUMBER){
+            //下溢出了，调整平衡
+            balance(parent,node,index);
+        }
+    }
+    private void balance(Node parent,Node x,int i){
+        //case6 根节点
+        if(x==root){
+            if(root.keyCount==0&&root.children[0]!=null){
+                root=root.children[0];
+            }
+            return;
+        }
+        Node leftSibling=parent.childLeftSibling(i);
+        Node rightSibling=parent.childRightSibling(i);
+        //case5-1:左边兄弟可以借，右旋
+         if(leftSibling!=null&&leftSibling.keyCount>MIN_KEY_NUMBER){
+             x.insertKey(parent.keys[i-1],0);//父亲下来
+             if(!leftSibling.isLeaf){//如果兄弟不是叶子，有孩子，在下一步之后兄弟会少一个孩子
+                 x.insertChild(leftSibling.removeRightmostChild(),0);//把兄弟最右边的孩子给被调整节点
+             }
+             parent.keys[i-1]=leftSibling.removeRightmostKey();//兄弟上去
+             return;
+         }
+        //case5-2:右边兄弟可以借，左旋
+        if(rightSibling!=null&&rightSibling.keyCount>MIN_KEY_NUMBER){
+            x.insertKey(parent.keys[i],x.keyCount);//父亲下来
+            if(!rightSibling.isLeaf){
+                x.insertChild(rightSibling.removeLeftmostChild(),x.keyCount+1);
+            }
+            parent.keys[i]=x.removeLeftmostKey();//兄弟上去
+        }
+        //case5-3 两边都不够借，向左合并
+        //a.x有左兄弟-合并到左兄弟
+        if(leftSibling!=null){
+            parent.removeChild(i);
+            leftSibling.insertKey(parent.removeKey(i-1),leftSibling.keyCount);
+            x.moveToTarget(leftSibling);//赋值当前节点所有的key,child到目标节点
+        }else{//b.x没有左兄弟-合并到x自己，移除右边兄弟
+            parent.removeChild(i+1);
+            x.insertKey(parent.removeKey(i),x.keyCount);
+            rightSibling.moveToTarget(x);
+        }
+    }
+
 }
 
