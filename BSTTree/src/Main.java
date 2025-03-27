4ff9a609b7447f820803b8c0cb1df37963c2e318
Always-Swiftie <mebuster06@gmail.com>
2025-03-27 11:03:36 +0800

    final

--- BSTTree/src/Main.java
+++ BSTTree/src/Main.java
@@ -1,14 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        BTree tree1=new BTree(2);
-        tree1.put(1);
-        tree1.put(2);
-        System.out.println(tree1.contains(1));
-        System.out.println(tree1.contains(2));
-        System.out.println(tree1.contains(3));
-        tree1.put(4);
-        tree1.put(5);
-        tree1.put(6);
-        System.out.println(tree1.contains(4));
+        BTree tree = new BTree(2);
+
+        tree.put(1);
+        tree.put(2);
+        tree.put(3);
+        tree.put(4);
+        tree.put(5);
+        tree.put(6);
+        tree.put(7);
+        tree.put(8);
+        tree.put(9);
+
+        System.out.println("Before Deletion:");
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("Contains " + i + "? " + tree.contains(i));
+        }
+
+        tree.remove(5);
+        tree.remove(6);
+        tree.remove(7);
+
+        System.out.println("After Deletion:");
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("Contains " + i + "? " + tree.contains(i));
+        }
+        }
+
     }
-}
